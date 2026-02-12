5a8fbd260cc17266f641b3c444bcf9dd44c9c563
himanshkumar6 <githubhimanshu0@gmail.com>
2026-02-12 14:45:13 +0530

    after A long time

--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import prerender from "@prerenderer/rollup-plugin";
-import puppeteerRenderer from "@prerenderer/renderer-puppeteer";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
   build: {
     rollupOptions: {
-      plugins: [
-        prerender({
-          routes: [
-            "/",
-            "/compress-pdf-to-200kb",
-            "/split-pdf",
-            "/pdf-to-jpg",
-            "/remove-metadata-from-pdf",
-            "/about",
-            "/contact",
-          ],
-          renderer: new puppeteerRenderer({
-            headless: true, // ✅ fixed
-            renderAfterDocumentEvent: "render-event",
-            launchOptions: {
-              args: [
-                "--no-sandbox",
-                "--disable-setuid-sandbox",
-                "--disable-dev-shm-usage",
-                "--disable-gpu",
-              ],
-            },
-          }),
-        }),
-      ],
       output: {
         manualChunks: {
           pdfjs: ["pdfjs-dist"],
